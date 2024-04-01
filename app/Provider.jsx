20d6556d29cda52119bf02c568d529edfff79a41
Rohit Kumar <145243903+kumarrohitdev@users.noreply.github.com>
2024-04-01 17:49:18 +0530

    Update Provider.jsx

--- app/Provider.jsx
+++ app/Provider.jsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from "next-themes";
 const CustomThemeProvider = ({ children }) => {
   return (
     <div className="transition-all delay-300">
-      <ThemeProvider defaultTheme="dark" attribute="class">
+      <ThemeProvider defaultTheme="system" attribute="class">
         {children}
       </ThemeProvider>
     </div>
